375129953d3e908111ed9df403e916d2138b1524
Rory Flint <roryflintphoto@gmail.com>
2020-03-02 22:37:18 +0000

    Psuedo for JS implementation

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log('hello!')
+/*
+
+MVP:
+Check current date.
+
+Check localstorage to see if there is a quote stored that matches today's date.
+If localstorage and date matches, use localstorage to populate the DOM.
+
+If localstorage and date mismatched OR no localstorage, make API call using
+today's date. Save to local storage. 
+
+Also, check the length of the quote. If it exceeds n length (yet to be decided),
+alter the text size of the quote itself to stop any kind of scrolling. 
+
+*/
+
+
+
+
+
+/*
+STRETCH:
+Implement small and subtle slide in menu to adjust theme, see github source/credit and be able to tweet the quote of the day. 
+*/

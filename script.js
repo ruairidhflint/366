876014b99b4a84f4170e65bad176f54c8ceaefaf
Rory Flint <roryflintphoto@gmail.com>
2020-03-02 23:14:16 +0000

    Add basic fetch method with error backup quote

--- script.js
+++ script.js
@@ -11,12 +11,39 @@ today's date. Save to local storage.
 Also, check the length of the quote. If it exceeds n length (yet to be decided),
 alter the text size of the quote itself to stop any kind of scrolling. 1
 */
-console.log('hello')
+const datefns = require('date-fns');
 
+const quote = document.querySelector('h1');
+const author = document.querySelector('h3');
 
+const errorQuote = {
+  quote: 'Find what you love and let it kill you.',
+  author: 'Charles Bukowski',
+};
 
+const today = new Date();
+const dayOfYear = datefns.getDayOfYear(today);
 
+fetch(`https://threesixsixquotes.herokuapp.com/quotes/${dayOfYear}`)
+  .then(res => res.json())
+  .then(res => {
+    setTextToDom(res);
+  })
+  .catch(error => {
+    console.log(error);
+    setTextToDom(errorQuote);
+  });
 
+function setTextToDom(content) {
+  const quoteText = content.quote;
+  const authorText = content.author;
+
+  if (quoteText.length > 93) {
+    quote.classList.toggle('reduced');
+  }
+  quote.textContent = quoteText;
+  author.textContent = authorText;
+}
 /*
 STRETCH:
 Implement small and subtle slide in menu to adjust theme, see github source/credit and be able to tweet the quote of the day. 
